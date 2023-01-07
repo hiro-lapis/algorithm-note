cbe413b019082b9515c7cf2628da03de21434c5d
hiro-lapis <coolish555ddddd@gmail.com>
2023-01-07 14:17:01 +0900

    add alternative approach

--- Atcoder/BootCamp/rally/rally.go
+++ Atcoder/BootCamp/rally/rally.go
@@ -9,29 +9,18 @@ import (
  * https://atcoder.jp/contests/abc156/tasks/abc156_c
  */
 func main() {
-	// 1 <= N <= 100 なので、会議を開く座標P も100 まで
-
-	// n = 人数
 	var n, tmp int
 	fmt.Scan(&n)
-	// x = 各自の座標
 	x := make([]int, n)
-	// 人数分の位置情報を受け取る
-	// TODO: 関数化する場合に、fmt を参照渡しした方がいい？
 	for i := 0; i < n; i++ {
 		fmt.Scan(&tmp)
 		x[i] = tmp
 	}
 
+	// Average approach
+	ans := AvgApproach(n, x)
 	// brute force
-	ans := int(1e18)
-	for i := 0; i < 100; i++ {
-		var life int
-		for j := 0; j < n; j++ {
-			life += int(math.Pow(float64(x[j]-i), 2.0))
-		}
-		ans = min(ans, life)
-	}
+	//ans := BruteForce(n, x)
 	fmt.Println(ans)
 }
 
@@ -40,4 +29,29 @@ func min(x, y int) int {
 		return y
 	}
 	return x
+}
+
+func AvgApproach(n int, x []int) (ans int) {
+	var sum, avg int
+	for i := 0; i < n; i++ {
+		sum += x[i]
+	}
+	// for decimal calculation, dividing with cast
+	avg = int(math.Round(float64(sum) / float64(n)))
+	for j := 0; j < n; j++ {
+		ans += int(math.Pow(float64(x[j]-avg), 2.0))
+	}
+	return
+}
+
+func BruteForce(n int, x []int) (ans int) {
+	ans = int(1e18)
+	for i := 0; i < 100; i++ {
+		var life int
+		for j := 0; j < n; j++ {
+			life += int(math.Pow(float64(x[j]-i), 2.0))
+		}
+		ans = min(ans, life)
+	}
+	return
 }
