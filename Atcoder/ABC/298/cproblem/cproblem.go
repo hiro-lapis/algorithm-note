eb20375eb2263ff030db79740977688830a70dac
hiro-lapis <coolish555ddddd@gmail.com>
2023-05-03 20:04:03 +0900

    55 done

---
+++ Atcoder/ABC/298/cproblem/cproblem.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+/** util func */
+func getScanner(fp *os.File) *bufio.Scanner {
+	// create buffered scanner
+	scanner := bufio.NewScanner(fp)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, 1000005), 1000005)
+	return scanner
+}
+
+func getNextString(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func getNextInt(scanner *bufio.Scanner) int {
+	i, _ := strconv.Atoi(getNextString(scanner))
+	return i
+}
+
+func getNextInt64(scanner *bufio.Scanner) int64 {
+	i, _ := strconv.ParseInt(getNextString(scanner), 10, 64)
+	return i
+}
+
+func getNextUint64(scanner *bufio.Scanner) uint64 {
+	i, _ := strconv.ParseUint(getNextString(scanner), 10, 64)
+	return i
+}
+
+func getNextFloat64(scanner *bufio.Scanner) float64 {
+	i, _ := strconv.ParseFloat(getNextString(scanner), 64)
+	return i
+}
+
+/*
+ * https://atcoder.jp/contests/abc298/tasks/abc298_c
+ */
+func main() {
+	fp := os.Stdin
+	wfp := os.Stdout
+	scanner := getScanner(fp)
+	writer := bufio.NewWriter(wfp)
+
+	// receive input
+	n := getNextInt(scanner)
+	l := make([][]int, n)
+	for i := 1; i <= n; i++ {
+
+	}
+	q := getNextInt(scanner)
+	for i := 0; i < q; i++ {
+		query := getNextInt(scanner)
+		i := getNextInt(scanner)
+		var j int
+		if query == 1 {
+			j = getNextInt(scanner)
+		}
+
+		if query == 1 {
+
+		}
+
+	}
+	fmt.Println(v)
+
+	defer writer.Flush()
+}
