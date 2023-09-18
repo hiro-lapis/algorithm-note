1fb21090717d063b8baab0bd8dab0dfdc4239ca5
hiro-lapis <coolish555ddddd@gmail.com>
2023-09-19 08:07:58 +0900

    309 abc

---
+++ Atcoder/ABC/309/bproblem/bproblem.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+/** util func */
+func getScanner(fp *os.File) *bufio.Scanner {
+	// create buffered scanner
+	scanner := bufio.NewScanner(fp)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, 1000005), 1000005)
+	return scanner
+}
+
+func getNextString(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func getNextInt(scanner *bufio.Scanner) int {
+	i, _ := strconv.Atoi(getNextString(scanner))
+	return i
+}
+
+func getNextInt64(scanner *bufio.Scanner) int64 {
+	i, _ := strconv.ParseInt(getNextString(scanner), 10, 64)
+	return i
+}
+
+func getNextUint64(scanner *bufio.Scanner) uint64 {
+	i, _ := strconv.ParseUint(getNextString(scanner), 10, 64)
+	return i
+}
+
+func getNextFloat64(scanner *bufio.Scanner) float64 {
+	i, _ := strconv.ParseFloat(getNextString(scanner), 64)
+	return i
+}
+
+func main() {
+	fp := os.Stdin
+	wfp := os.Stdout
+	scanner := getScanner(fp)
+	writer := bufio.NewWriter(wfp)
+
+	// receive input
+	n := getNextInt(scanner)
+	a := make([]string, n)
+	for i := 0; i < n; i++ {
+		a[i] = getNextString(scanner)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if j == 0 {
+				bef := string(a[i][j])
+				af := string(a[i+1][j])
+			}
+
+		}
+	}
+	fmt.Println(v)
+
+	defer writer.Flush()
+}
