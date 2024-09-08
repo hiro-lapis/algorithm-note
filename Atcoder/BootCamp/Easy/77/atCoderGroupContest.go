1963cee1c121f1041b10dbf75925aa933603c99d
hiro-lapis <coolish555ddddd@gmail.com>
2024-09-08 23:37:34 +0900

    fix utility func

---
+++ Atcoder/BootCamp/Easy/77/atCoderGroupContest.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// https://atcoder.jp/contests/agc012/tasks/agc012_a
+func main() {
+	var n int
+	fmt.Scan(&n)
+	m := n * 3
+	a := make([]int, m)
+	for i := 0; i < m; i++ {
+		var tmp int
+		fmt.Scan(&tmp)
+		a[i] = tmp
+
+	}
+	// sort desc
+	//sort.Slice(a, func(i, j int) bool {
+	//	return a[i] > a[j]
+	//})
+	sort.Ints(a)
+	sum := 0
+
+	for i := 0; i < n; i++ {
+		// ↓の-1部分は 配列のlength,配列のindexの差分調整のための1
+		// ↓の+1部分は 2番目に大きい値を参照するための1
+		v := a[m-(2*i+1)-1]
+		// ↓のようにしてもいいが、異なる意味をもつ値を別に扱うため↑のようにすることを推奨
+		//v := a[m-(2*i)-2]
+		sum += v
+	}
+	fmt.Println(sum)
+}
